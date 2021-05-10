2b4f8ac50ecc3893fba2b7956e92fd2c6d838e73
SamElmore625 <54898090+SamElmore625@users.noreply.github.com>
2021-05-10 09:06:06 -0400

    Cont.

---
+++ MusicReviewV3/StartScreenViewController.swift
@@ -0,0 +1,24 @@
+//
+//  StartScreenViewController.swift
+//  MusicReviewV3
+//
+//  Created by user930813 on 5/10/21.
+//
+
+import UIKit
+
+class StartScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        }
+    
+    
+    @IBOutlet weak var SignUpOne: UIButton!{
+        let
+    }
+    
+    @IBOutlet weak var LogInOne: UIButton!
+    
+    
+    }
